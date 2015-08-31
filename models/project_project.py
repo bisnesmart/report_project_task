ad5186c83e9b875174f5df854bc0f5c67a2b5307
gonxi <gonxi.gonzalo@gmail.com>
2015-08-31 16:28:21 +0200

    commit inicial

---
+++ models/project_project.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# (c) 2015 bisnesmart
+# License AGPL-3 - See LICENSE file on root folder for details
+##############################################################################
+from openerp import models, fields, api
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
